10fe8d639093b0aab51423a519aa5aad2a6842aa
gorbatschow <gorbatschow.r@gmail.com>
2022-12-29 10:51:36 +0300

    fix methods names + codestyle

--- RtSoundBaseGen.h
+++ RtSoundBaseGen.h
@@ -6,14 +6,14 @@ class RtSoundBaseGen : public RtSoundClient {
 public:
   RtSoundBaseGen(int priority = 0);
 
-  inline void setInputEnbaled(bool value) { _inputEnabled.exchange(value); }
-  inline bool inputEnabled() const { return _inputEnabled; }
-
-  inline void setOutputEnabled(bool value) { _outputEnabled.exchange(value); }
-  inline bool outputEnabled() const { return _outputEnabled; }
+  inline void setInputEnabled(bool value) { _inputEnabled.exchange(value); }
+  inline bool inputEnabled() const { return _inputEnabled.load(); }
 
   inline void setInputChannel(int value) { _inputChannel.exchange(value); }
   inline int inputChannel() const { return _inputChannel.load(); }
+
+  inline void setOutputEnabled(bool value) { _outputEnabled.exchange(value); }
+  inline bool outputEnabled() const { return _outputEnabled.load(); }
 
   inline void setOutputChannel(int value) { _outputChannel.exchange(value); }
   inline int outputChannel() const { return _outputChannel.load(); }
@@ -23,11 +23,13 @@ protected:
 
 private:
   std::atomic_int _inputChannel{};
-  std::atomic_int _outputChannel{};
   std::atomic_bool _inputEnabled{false};
+
+  std::atomic_int _outputChannel{};
   std::atomic_bool _outputEnabled{false};
 
   virtual void receiveStreamData(const RtStreamData &streamData) override final;
+
   void fillInput(const RtStreamData &streamData) const;
   void fillOutput(const RtStreamData &streamData) const;
 };
